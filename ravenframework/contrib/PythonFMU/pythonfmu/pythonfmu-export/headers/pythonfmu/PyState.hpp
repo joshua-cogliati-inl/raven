f62af7357caf676db17a1aaee0d0188fd5335a3f
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2022-05-25 11:58:44 -0600

    Adding generic code to find library.

--- ravenframework/contrib/PythonFMU/pythonfmu/pythonfmu-export/headers/pythonfmu/PyState.hpp
+++ ravenframework/contrib/PythonFMU/pythonfmu/pythonfmu-export/headers/pythonfmu/PyState.hpp
@@ -19,7 +19,17 @@ public:
         if (!was_initialized_) {
             Py_SetProgramName(L"./PythonFMU");
             Py_Initialize();
-          dlopen("libpython3.7m.so", RTLD_NOW | RTLD_GLOBAL);
+            PyObject * apiflags = PySys_GetObject("abiflags");
+            if (PyUnicode_Check(apiflags)) {
+                wchar_t apiflags_w[16];
+                PyUnicode_AsWideChar(apiflags, apiflags_w, 16);
+                apiflags_w[16-1] = '\0';
+                char library[256];
+                snprintf(library, 256, "libpython%d.%d%ls.so", PY_MAJOR_VERSION, PY_MINOR_VERSION, apiflags_w);
+                library[256-1] = '\0';
+                //printf("python library: %s\n", library);
+                dlopen(library, RTLD_NOW | RTLD_GLOBAL);
+            }
             PyEval_InitThreads();
             _mainPyThread = PyEval_SaveThread();
         }
