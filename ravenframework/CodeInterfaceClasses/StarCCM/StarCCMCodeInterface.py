cd61cd1e62529d5f184523e9c783a5e40023fd46
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2022-11-22 11:57:59 -0700

    More work.

--- ravenframework/CodeInterfaceClasses/StarCCM/StarCCMCodeInterface.py
+++ ravenframework/CodeInterfaceClasses/StarCCM/StarCCMCodeInterface.py
@@ -18,6 +18,13 @@ This is a CodeInterface for the StarCCM+ code.
 
 """
 
+import os
+import glob
+import re
+import numpy as np
+import xarray as xr
+import pandas as pd
+
 from ..Generic.GenericCodeInterface import GenericCode
 
 class StarCCM(GenericCode):
@@ -25,3 +32,52 @@ class StarCCM(GenericCode):
     This class is used to run the starccm+ code
   """
 
+  def finalizeCodeOutput(self, command, codeLogFile, subDirectory):
+    """
+      Convert csv information to RAVEN's prefered formats
+      @ In, command, ignored
+      @ In, codeLogFile, ignored
+      @ In, subDirectory, string, the subdirectory where the information is.
+      @ Out, dictionary, dict, the returned information format:
+        {field : array-like} or {field : dict} (read by pd.DataFrame.from_dict)
+    """
+    outDict = {}
+    print("finalizeCodeOutput", command, codeLogFile, subDirectory)
+    names = glob.glob(os.path.join(subDirectory, "XYZ_Internal_Table_Fluid_table_*.csv"))
+    dataDict = {}
+    for name in names:
+      intRE = ".*_([0-9]+).csv"
+      floatRE = ".*_([0-9]+\.[0-9]*[eE][+-][0-9]+).csv"
+      intMatch = re.match(intRE, name)
+      if intMatch:
+        value = int(intMatch.group(1))
+      else:
+        floatMatch = re.match(floatRE, name)
+        if floatMatch:
+          value = float(floatMatch.group(1))
+      if not intMatch and not floatMatch:
+        print("no id found in",name)
+        continue
+      data = pd.read_csv(name)
+      dataDict[value] = data
+    #print("names", names)
+    #print("dataDict", dataDict)
+    sortedKeys = sorted(dataDict.keys())
+    print("keys", sortedKeys)
+    columns = dataDict[sortedKeys[0]].columns
+    for column in columns:
+      outDict[column] = []
+    outDict['index'] = []
+    outDict['autoindex'] = []
+    for key in sortedKeys:
+      data = dataDict[key]
+      length = data.shape[0]
+      outDict['index'].extend([key]*length)
+      prev = len(outDict['autoindex'])
+      outDict['autoindex'].extend(range(prev, prev+length))
+      for column in columns:
+        outDict[column].extend(data[column])
+    return outDict
+    #out = super().finalizeCodeOutput(command, codeLogFile, subDirectory)
+    #print("out", out, type(out))
+    #return out
