ff536756e379666b2d40ac52c304f51946662d66
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2025-02-06 13:33:37 -0700

    Track the population separately and then check the fitness at the end.

--- ravenframework/Optimizers/survivorSelection/survivorSelection.py
+++ ravenframework/Optimizers/survivorSelection/survivorSelection.py
@@ -39,6 +39,10 @@ def singleObjSurvivorSelect(self, info, rlz, traj, offSprings, offSpringFitness,
     @ In, offSprings, list, list of offsprings
     @ In, offSpringFitness, list, list of offspring fitness
   """
+  if self.fitness is not None:
+    for i in range(self.population.shape[0]):
+      self._sampledPopulationInfo[tuple(self.population[i].data)] = self.fitness.to_dataarray()[:,i]
+
   if self.counter > 1:
     self.population, self.fitness,\
     self.popAge,self.objectiveVal = self._survivorSelectionInstance(age=self.popAge,
@@ -65,6 +69,10 @@ def multiObjSurvivorSelect(self, info, rlz, traj, offSprings, offSpringFitness, 
     @ In, offSpringFitness, list, list of fitness values for offspring individuals
     @ In, objectiveVal, list, values of the objectives (for ranking and crowding distance calculation)
   """
+  if self.fitness is not None:
+    for i in range(self.population.shape[0]):
+      self._sampledPopulationInfo[tuple(self.population[i].data)] = self.fitness.to_dataarray()[:,i]
+
   if self.counter > 1:
     self.population,self.rank, \
     self.popAge,self.crowdingDistance, \
